9f0c8f867a10b18ca5557dc57abbdfdf6a3e98b5
Ayush Moon <159437729+ayushmoon0@users.noreply.github.com>
2026-02-05 19:39:15 +0530

    Revision: Day-31,32

---
+++ Milk_Shake/src/com/day30/Test.java
@@ -0,0 +1,8 @@
+package com.day30;
+
+public class Test {
+	public static void main(String[] args) {
+		//Taught nothing
+		System.out.println("Practical Day-1");
+	}
+}
---
+++ Milk_Shake/src/com/day31/Test.java
@@ -0,0 +1,8 @@
+package com.day31;
+
+public class Test {
+	public static void main(String[] args) {
+		//Today Nothing also taught
+		System.out.println("Practical Day-2");
+	}
+}
